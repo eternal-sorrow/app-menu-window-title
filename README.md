dcf3007a7acfcff819556bcd118614d021f566fb
Eternal Sorrow <sergamena@inbox.ru>
2013-10-18 17:22:29 +1100

    Initial #2. Added metadata.json and extension.js, wrote acceptable README.md

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-app-menu-window-title
-=====================
+Status Title Bar
+================
 
-Gnome-Shell extension. Shows the title of windows in the status bar instead of app name.
+Gnome-Shell extension that shows the current focused window's title. It doesn't
+replace the current AppButton, just changes it's label.
+
+This extension is based on
+[Status Title Bar] (https://github.com/emerinohdz/StatusTitleBar)
+extension, written by
+[Edgar Merino] (https://github.com/emerinohdz),
+but is much more simple.
+
+Note
+----
+
+For best results, use the extension
+[Extend left box] (https://extensions.gnome.org/extension/51/extend-left-box)
+by [Stephen Zhang] (https://github.com/StephenPCG)
+.
+
+Installation
+------------
+
+    cd ~/.local/share/gnome-shell/extensions
+    git clone https://github.com/eternal-sorrow/app-menu-window-title.git
+    mv app-menu-window-title app-menu-window-title@eternal-sorrow.github.com 
+
+Author
+------
+
+[Eternal Sorrow](https://github.com/eternal-sorrow) (sergmena at mail dot ru)
---
+++ extension.js
@@ -0,0 +1,217 @@
+/**
+ * app-menu-window-title extension
+ * @autor: eternal-sorrow <sergamena at mail dot ru>
+
+ * Based on StatusTitleBar extension,written by @emerino
+ *
+ * This extension makes the AppMenuButton show the title of
+ * the current focused window,instead of the application's name.
+ *
+ */
+
+const Lang = imports.lang;
+
+const Shell = imports.gi.Shell;
+const Main = imports.ui.main;
+const Signals = imports.signals;
+
+const app_menu_window_title_handler = new Lang.Class
+({
+	Name: 'app_menu_window_title_handler',
+
+	_init: function(app_menu)
+	{
+
+		this.app_menu=app_menu;
+		
+		this.starting_apps = [];
+		
+		this.app_menu_changed = false;
+
+		/*** Holders for local tracked signals ***/
+		this.main_signals = [];
+		this.ws_signals = [];
+		this.target_app_signals = [];
+		
+
+		this.connect_and_track
+		(
+			this.main_signals,
+			Shell.WindowTracker.get_default(),
+			'notify::focus-app',
+			Lang.bind(this,this.on_focus_app_changed)
+		);
+
+		this.connect_and_track
+		(
+			this.main_signals,
+			global.window_manager,
+			'switch-workspace',
+			Lang.bind(this,this._sync)
+		);
+
+		this.connect_and_track
+		(
+			this.main_signals,
+			global.screen,
+			'notify::n-workspaces',
+			Lang.bind(this,this.on_change_n_workspaces)
+		);
+
+		this.connect_and_track
+		(
+			this.main_signals,
+			this.app_menu,
+			'changed',
+			Lang.bind(this,this.on_app_menu_changed)
+		);
+
+
+		this.on_change_n_workspaces();
+	},
+	
+	/** Utility functions **/
+	connect_and_track: function (signals,subject,name,cb)
+	{
+		signals.push([subject,subject.connect(name,cb)]);
+	},
+
+	disconnect_tracked_signals: function(signals)
+	{
+		if(signals.length<1)
+			return;
+		signals.forEach(function(sig){sig[0].disconnect(sig[1]);});
+	},
+
+
+	on_focus_app_changed: function()
+	{
+		if(!Shell.WindowTracker.get_default().focus_app)
+		{
+			// If the app has just lost focus to the panel, pretend
+			// nothing happened; otherwise you can't keynav to the
+			// app menu.
+			if(global.stage_input_mode == Shell.StageInputMode.FOCUSED)
+				return;
+		}
+		this._sync();
+	},
+	
+	on_change_n_workspaces: function()
+	{
+		this.disconnect_tracked_signals(this.ws_signals);
+
+		for(let i = 0;i<global.screen.n_workspaces;++i )
+			this.connect_and_track
+			(
+				this.ws_signals,
+				global.screen.get_workspace_by_index(i),
+				'window-removed',
+				Lang.bind(this,this._sync)
+			);
+	},
+
+
+	on_window_title_changed: function(win)
+	{
+		if(win.has_focus())
+		{
+			this.app_menu._label.setText(win.title);
+		}
+	},
+
+	on_app_menu_changed: function()
+	{
+		this.app_menu_changed=true;
+		this._sync();
+		this.app_menu_changed=false;
+	},
+	
+
+
+	/** Actually: sync  **/
+	_sync: function()
+	{
+		let win = global.display.focus_window;
+		
+		if(win == null)
+			return;
+
+		if(!win._app_menu_win_ttl_chnd_sig_id_)
+			this.init_window(win);
+
+		this.app_menu._label.setText(win.title);
+
+		this.disconnect_tracked_signals(this.target_app_signals);
+
+		if(!this.app_menu_changed)
+			this.app_menu.emit('changed');
+	},
+
+
+
+	init_window: function(win)
+	{
+		if(win._app_menu_win_ttl_chnd_sig_id_)
+		{
+			win.disconnect(win._app_menu_win_ttl_chnd_sig_id_);
+		}
+
+		win._app_menu_win_ttl_chnd_sig_id_ = win.connect
+		(
+			"notify::title",
+			Lang.bind(this,this.on_window_title_changed)
+		);
+	},
+
+	destroy: function()
+	{
+		// disconnect signals
+		this.disconnect_tracked_signals(this.main_signals);
+
+		// any signals from on_change_n_workspaces
+		this.disconnect_tracked_signals(this.ws_signals);
+
+		// any signals from init_window.
+		// _sync requires the _app_menu_win_ttl_chnd_sig_id_.
+		let windows = global.get_window_actors();
+		for (let i = 0; i < windows.length; ++i)
+		{
+			let win = windows[i];
+			if(win._app_menu_win_ttl_chnd_sig_id_)
+			{
+				win.disconnect(win._app_menu_win_ttl_chnd_sig_id_);
+				delete win._app_menu_win_ttl_chnd_sig_id_;
+			}
+		}
+
+		// any signals from _sync
+		this.disconnect_tracked_signals(this.target_app_signals);
+
+		// Call parent destroy.
+		this.parent();
+	}
+});
+
+Signals.addSignalMethods(app_menu_window_title_handler.prototype);
+
+// lightweight object,acts only as a holder when ext disabled
+let handler = null; 
+
+function init()
+{
+
+}
+
+function enable()
+{
+	handler = new app_menu_window_title_handler
+	(
+		Main.panel.statusArea.appMenu
+	);
+}
+
+function disable()
+{
+	handler.destroy();
+}
